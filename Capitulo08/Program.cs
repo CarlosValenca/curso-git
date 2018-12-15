316ad6b4fb49b59f79898cb6ecdee17bdeb7a9d6
Carlos Valença <carlos_valenca@uol.com.br>
2018-12-15 16:56:52 -0200

    Projeto iniciado mostrando a mensagem Ola Mundo

---
+++ Capitulo08/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Capitulo08
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Nova Mensagem");
+        }
+    }
+}
